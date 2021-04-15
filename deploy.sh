3047f89b9364a544b7ede52c55bb8d691ce35e6a
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-15 01:26:34 -0400

    CI/CD worflow

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 cd /home/streaming/webapps/events-frontend
-git pull https:/github.com/Streaming-Cuba/events-frontend.git
+su
+git pull https://github.com/Streaming-Cuba/events-frontend.git
 pm2 stop events-frontend
 yarn 
 yarn build
