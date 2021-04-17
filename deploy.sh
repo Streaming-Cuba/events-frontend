aaa4560fea8a90a710c0f63e8edcce1fcb61a792
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-17 00:08:04 -0400

    Custom error pages

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 cd /home/streaming/webapps/events-frontend
-su
 git pull https://github.com/Streaming-Cuba/events-frontend.git
 pm2 stop events-frontend
 yarn 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,81 +1,94 @@
-import React, { useEffect } from 'react';
-import { theme } from '../config';
-import Head from 'next/head';
-import { ThemeProvider, CssBaseline } from '@material-ui/core';
-import Navbar from '../partials/Navbar';
-import Footer from '../partials/Footer';
-import { DefaultSeo } from 'next-seo';
+import React, { useEffect } from "react";
+import { theme } from "../config";
+import Head from "next/head";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import Navbar from "../partials/Navbar";
+import Footer from "../partials/Footer";
+import { DefaultSeo } from "next-seo";
+import { NextWebVitalsMetric } from "next/app";
 
-function MyApp(props: MyAppProps) {
-  useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
+class MyApp extends React.Component<MyAppProps> {
+  constructor(props: MyAppProps) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement?.removeChild(jssStyles);
     }
-  }, []);
+  }
 
-  return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+  render() {
+    return (
+      <>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+          <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+          <link rel="stylesheet" href="/styles/pace.css"></link>
+          <link href="/images/meeting-room.png" rel="icon" />
+          <link href="/styles/font.css" rel="stylesheet" />
+          <link href="/styles/styles.css" rel="stylesheet" />
+        </Head>
+        <DefaultSeo
+          openGraph={{
+            type: "website",
+            locale: "es",
+            url: "https://eventos.streamingcuba.com",
+            site_name: "Eventos",
+            title: "Eventos",
+            description: "SEO made easy for Next.js projects",
+          }}
+          title="Eventos"
         />
-        <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
-        <link
-          rel="stylesheet"
-          href="/styles/pace.css"
-        ></link>
-        <link href="/images/meeting-room.png" rel="icon" />
-        <link href="/styles/font.css" rel="stylesheet" />
-        <link href="/styles/styles.css" rel="stylesheet" />
-      </Head>
-      <DefaultSeo
-        openGraph={{
-          type: 'website',
-          locale: 'es',
-          url: 'https://eventos.streamingcuba.com',
-          site_name: 'Eventos',
-          title: 'Eventos',
-          description: 'SEO made easy for Next.js projects',
-        }}
-      />
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Navbar />
-        <props.Component {...props.pageProps} />
-        <Footer />
-      </ThemeProvider>
-    </>
-  );
+        {this.props.error ? (
+          <this.props.Component {...this.props.pageProps} />
+        ) : (
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Navbar />
+            <this.props.Component {...this.props.pageProps} />
+            <Footer />
+          </ThemeProvider>
+        )}
+      </>
+    );
+  }
+
+  static async getInitialProps({ err, ctx }){
+    const req = ctx.req;
+    const pathname = ctx.pathname;
+    const res = ctx.res;
+  
+    /**
+     * Abort if one var is not present.
+     * For example, the req obj will be undefined if we don't
+     * have a page reload but a page switch via the Next Router.
+     */
+    if (!req || !pathname || !res) {
+      return {};
+    }
+
+    
+    const hasError = res && res.statusCode >= 400;
+    return {error: hasError};
+  };
+  
 }
 
 interface MyAppProps {
   Component: any;
   pageProps: any;
   user: any;
+  error: boolean;
 }
 
-/**
- * Fetch some data server side before rendering the page client side.
- *
- * @param {object} context
- *   The context object.
- */
-MyApp.getInitialProps = async ({ ctx }) => {
-  const req = ctx.req;
-  const pathname = ctx.pathname;
-  const res = ctx.res;
-
-  /**
-   * Abort if one var is not present.
-   * For example, the req obj will be undefined if we don't
-   * have a page reload but a page switch via the Next Router.
-   */
-  if (!req || !pathname || !res) {
-    return {};
-  }
-  return {};
-};
-
 export default MyApp;
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric)
+  //TODO: Send it to GOogle Analytics
+}
