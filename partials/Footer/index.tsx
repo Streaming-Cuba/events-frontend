9dfc4d2a47b910e4ebef73273eebc5695f355023
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-18 15:25:21 -0400

    Automation test

--- partials/Footer/index.tsx
+++ partials/Footer/index.tsx
@@ -73,7 +73,7 @@ function Footer() {
       <div className={classes.copyright}>
         <div className={clsx(classes.container, classes.copyrightContent)}>
           <a href="https://streamingcuba.com" className={classes.logo}>
-            <span>StreamingCuba y la vieja del hacha</span>
+            <span>StreamingCuba v222</span>
           </a>
           <p>
             Copyright © {new Date().getFullYear()} StreamingCuba - All rights
