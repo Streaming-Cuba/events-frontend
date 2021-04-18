61ebf3ac557c5ed201e15e8d2c61a34866e9fb91
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-18 14:25:05 -0400

    New deply flow

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-# cd /home/streaming/apps/events-frontend
-# git pull https://github.com/Streaming-Cuba/events-frontend.git
-# pm2 restart events-frontend
+cd /home/streaming/apps/events-frontend
+git pull https://github.com/Streaming-Cuba/events-frontend.git
+pm2 restart events-frontend
