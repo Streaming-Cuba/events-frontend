960f1e5ee4a1c962ec163ea07fc5b3b0c09a0375
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-18 14:10:36 -0400

    Flow updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-cd /home/streaming/webapps/events-frontend
-git pull https://github.com/Streaming-Cuba/events-frontend.git
-pm2 stop events-frontend
-yarn 
-yarn build
-pm2 restart events-frontend
+# cd /home/streaming/apps/events-frontend
+# git pull https://github.com/Streaming-Cuba/events-frontend.git
+# pm2 restart events-frontend
