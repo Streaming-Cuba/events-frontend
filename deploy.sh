86235b088d68918233ac2436c43b6845bfe16c65
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-20 16:04:09 -0400

    New flow

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cd /home/streaming/apps/events-frontend
-git pull https://github.com/Streaming-Cuba/events-frontend.git
-pm2 restart events-frontend
+sudo git pull https://github.com/Streaming-Cuba/events-frontend.git
+sudo pm2 restart events-frontend
