68890796501f2584406948608ac42fd0ff0f6afb
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-25 16:58:57 -0400

    Sidebar

--- src/partials/Sidebar/index.tsx
+++ src/partials/Sidebar/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { Drawer } from "@material-ui/core";
+import { Drawer, List, ListItem, ListItemText } from "@material-ui/core";
+import Link from "next/link";
 
 import { useTypedSelector } from "../../apis/redux";
 
 function Sidebar() {
+  const { isSidebarOpen } = useTypedSelector((state) => state.layout);
 
-    const {isSidebarOpen} = useTypedSelector(state => state.layout);
-
-    return (
-        <Drawer open={isSidebarOpen}>
-asdasda
-        </Drawer>
-    )
+  return (
+    <Drawer open={isSidebarOpen}>
+      <List component="nav">
+        <ListItem button component={Link} href="/">
+          <ListItemText primary="Inicio" />
+        </ListItem>
+        <ListItem button component={Link} href="/events">
+          <ListItemText primary="Eventos" />
+        </ListItem>
+        <ListItem button component={Link} href="/about-us">
+          <ListItemText primary="Acerca de" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 }
 
 export default Sidebar;
