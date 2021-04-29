2c113d75dbc72459d4d10d5de9ca18aae276d629
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-28 21:46:34 -0400

    Always accept remote on Git Pull

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 cd /home/streaming/apps/events-frontend
-sudo git pull https://github.com/Streaming-Cuba/events-frontend.git
+sudo git pull https://github.com/Streaming-Cuba/events-frontend.git -s recursive -X theirs
 yarn --ignore-engines
 pm2 restart events-frontend
