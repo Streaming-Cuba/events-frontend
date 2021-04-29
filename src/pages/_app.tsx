6b59d4d07846de6cf483e6338daae63dd42e6d4b
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-28 20:46:39 -0400

    Add fav icons and GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,81 +39,81 @@ class MyApp extends React.Component<MyAppProps> {
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="/icons/apple-icon-57x57.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-57x57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="60x60"
-            href="/icons/apple-icon-60x60.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-60x60.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="/icons/apple-icon-72x72.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/icons/apple-icon-76x76.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="/icons/apple-icon-114x114.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="/icons/apple-icon-120x120.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="/icons/apple-icon-144x144.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="/icons/apple-icon-152x152.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/icons/apple-icon-180x180.png"
+            href="https://eventos.streamingcuba.com/icons/apple-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/icons/android-icon-192x192.png"
+            href="https://eventos.streamingcuba.com/icons/android-icon-192x192.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/icons/favicon-32x32.png"
+            href="https://eventos.streamingcuba.com/icons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
-            href="/icons/favicon-96x96.png"
+            href="https://eventos.streamingcuba.com/icons/favicon-96x96.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/icons/favicon-16x16.png"
+            href="https://eventos.streamingcuba.com/icons/favicon-16x16.png"
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="https://eventos.streamingcuba.com/icons/ms-icon-144x144.png"
           />
           <link rel="manifest" href="/manifest.json" />
           <meta
             name="msapplication-TileColor"
             content={theme.palette.primary.main}
           />
-          <meta
-            name="msapplication-TileImage"
-            content="/icons/ms-icon-144x144.png"
-          ></meta>
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
@@ -147,7 +147,9 @@ class MyApp extends React.Component<MyAppProps> {
             images: [
               {
                 url:
-                  "https://eventos.streamingcuba.com/icons/android-icon-48x48.png",
+                  "https://eventos.streamingcuba.com/icons/apple-icon-60x60.png",
+                width: 60,
+                height: 60,
               },
             ],
           }}
