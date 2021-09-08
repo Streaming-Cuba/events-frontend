9df9f6e205f3c15b3cc81d6a288c7e974c4085c7
Jesus Reikel <68043863+jesusr00@users.noreply.github.com>
2021-09-08 18:14:10 -0400

    fix image size error

--- src/utils/YoutubeUtils.ts
+++ src/utils/YoutubeUtils.ts
@@ -1,5 +1,4 @@
 export function getVideoImageURL (videURL?: string): string {
   return videURL?
-    `http://img.youtube.com/vi/${videURL.split("https://youtu.be/")[1]}/0.jpg`:
-    "https://www.iconsdb.com/icons/download/white/video-play-512.png";
+    `http://img.youtube.com/vi/${videURL.split("https://youtu.be/")[1]}/0.jpg`: null;
 }
