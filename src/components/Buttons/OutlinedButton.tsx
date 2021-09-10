1fb39593a6342905d08e9f1fccac832d9318cf8e
Jesús Reikel López Martín <68043863+jesusr00@users.noreply.github.com>
2021-09-10 19:41:33 -0400

    change background size (#10)

--- src/components/Buttons/OutlinedButton.tsx
+++ src/components/Buttons/OutlinedButton.tsx
@@ -1,5 +1,5 @@
-import { Button, makeStyles } from '@material-ui/core';
-import { useRouter } from 'next/router';
+import { Button, makeStyles } from "@material-ui/core";
+import { useRouter } from "next/router";
 
 function OutlinedButton(props: { children: React.ReactNode; href?: string }) {
   const classes = useStyles();
@@ -18,18 +18,18 @@ function OutlinedButton(props: { children: React.ReactNode; href?: string }) {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    background: '0 0',
-    border: '2px solid #fff',
-    color: '#fff',
-    textTransform: 'uppercase',
+    background: "rgba(0,0,0,0.5)",
+    border: "2px solid #fff",
+    color: "#fff",
+    textTransform: "uppercase",
     fontWeight: 500,
-    padding: '11px 40px',
-    display: 'inline-block',
-    borderRadius: '25px',
-    margin: '10px',
+    padding: "11px 40px",
+    display: "inline-block",
+    borderRadius: "25px",
+    margin: "10px",
 
-    '&:hover': {
-      background: '#0006',
+    "&:hover": {
+      background: "rgba(0,0,0,0.9)",
     },
   },
 }));
